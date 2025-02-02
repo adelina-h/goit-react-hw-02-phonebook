4c50598387a888ff866238315d15443a00e91d97
Adelina-hub <adelina.macarascu@gmail.com>
2025-02-02 22:12:59 +0200

    hw done again

--- src/components/Filter/Filter.jsx
+++
@@ -1,8 +0,0 @@
-import { SearchLabel, SearchInput } from './FilterStyled';
-
-export const Filter = ({ value, onChange }) => (
-  <SearchLabel>
-    Find contacts by name
-    <SearchInput type="text" value={value} onChange={onChange} />
-  </SearchLabel>
-);
